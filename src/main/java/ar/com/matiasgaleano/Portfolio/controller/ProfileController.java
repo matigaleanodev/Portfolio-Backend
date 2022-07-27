89fa2624a88911ae57a2121100a0cc9b9d201d7a
Matias Ariel Galeano <galeano_matias217@yahoo.com.ar>
2022-07-27 14:32:24 -0300

    arreglo profile controller

--- src/main/java/ar/com/matiasgaleano/Portfolio/controller/ProfileController.java
+++ src/main/java/ar/com/matiasgaleano/Portfolio/controller/ProfileController.java
@@ -10,31 +10,32 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = "*")
+@RequestMapping("/api")
 public class ProfileController {
-  
+
   @Autowired
   private IProfileService profServ;
-  
+
   @GetMapping("/profile")
-  public ResponseEntity<List<Profile>> getProfile(){
+  public ResponseEntity<List<Profile>> getProfile() {
     return new ResponseEntity(profServ.getProfile(), HttpStatus.OK);
   }
-  
+
   @PostMapping("/profile")
-  public ResponseEntity<?> postProfile(Profile prof){
+  public ResponseEntity<?> postProfile(Profile prof) {
     profServ.postProfile(prof);
     return new ResponseEntity(new Message("Perfil Creado"), HttpStatus.OK);
   }
-  
+
   @PutMapping("/profile")
-  public ResponseEntity<?> editProfile(Profile prof){
+  public ResponseEntity<?> editProfile(Profile prof) {
     profServ.editProfile(prof);
     return new ResponseEntity(new Message("Perfil Editado"), HttpStatus.OK);
-  } 
-  
-}
+  }
 
+}
