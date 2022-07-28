4bd3535a6c4673022f9faab6bd36695034306518
Matias Ariel Galeano <galeano_matias217@yahoo.com.ar>
2022-07-28 20:42:51 -0300

    configuracion cors final

--- src/main/java/ar/com/matiasgaleano/Portfolio/PortfolioApplication.java
+++ src/main/java/ar/com/matiasgaleano/Portfolio/PortfolioApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PortfolioApplication {
@@ -16,5 +18,14 @@ public class PortfolioApplication {
   public static void main(String[] args) {
     SpringApplication.run(PortfolioApplication.class, args);
   }
+  
+  public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("*");
+			}
+		};
+	}
 
 }
