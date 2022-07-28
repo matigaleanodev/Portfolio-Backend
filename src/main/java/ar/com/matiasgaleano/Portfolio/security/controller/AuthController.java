ed88b6c1f2633449e0041ba37b19b5f3cdeb1cd0
Matias Ariel Galeano <galeano_matias217@yahoo.com.ar>
2022-07-28 17:58:15 -0300

    arreglo cors origin

--- src/main/java/ar/com/matiasgaleano/Portfolio/security/controller/AuthController.java
+++ src/main/java/ar/com/matiasgaleano/Portfolio/security/controller/AuthController.java
@@ -4,12 +4,14 @@ import ar.com.matiasgaleano.Portfolio.security.model.JwtUser;
 import ar.com.matiasgaleano.Portfolio.security.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/auth")
 public class AuthController {
 
