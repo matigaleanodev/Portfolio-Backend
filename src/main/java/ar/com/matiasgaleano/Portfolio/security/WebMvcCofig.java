325ad10579d30a31553d6d46984be3133ee3677d
Matias Ariel Galeano <galeano_matias217@yahoo.com.ar>
2022-07-28 17:46:58 -0300

    arreglo cors origin

---
+++ src/main/java/ar/com/matiasgaleano/Portfolio/security/WebMvcCofig.java
@@ -0,0 +1,17 @@
+package ar.com.matiasgaleano.Portfolio.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+    public class WebMvcCofig implements WebMvcConfigurer{
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/*")
+                    .allowedOrigins("*")
+                    .allowedMethods("*")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
+        }
+    }
