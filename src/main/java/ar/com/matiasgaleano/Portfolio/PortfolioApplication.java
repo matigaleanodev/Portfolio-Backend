57f2cd61e67d8e527d14b54b6fd459175ec1f8b2
Matias Ariel Galeano <galeano_matias217@yahoo.com.ar>
2022-07-29 13:48:18 -0300

    cors en authentication filter

--- src/main/java/ar/com/matiasgaleano/Portfolio/PortfolioApplication.java
+++ src/main/java/ar/com/matiasgaleano/Portfolio/PortfolioApplication.java
@@ -19,13 +19,13 @@ public class PortfolioApplication {
     SpringApplication.run(PortfolioApplication.class, args);
   }
   
-  public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/*").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
-			}
-		};
-	}
+//  public WebMvcConfigurer corsConfigurer() {
+//		return new WebMvcConfigurer() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/*").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
+//			}
+//		};
+//	}
 
 }
